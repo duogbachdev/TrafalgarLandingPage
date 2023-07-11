09e14a18d29ee2dec4dc861e61774dc7a190a2de
duogbachdev <contact.duogbachdev@gmail.com>
2023-07-11 17:17:01 +0700

    end_TraFalGar_notResp

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         slideDown: 'slideDown .4s ease-in-out',
         slideUp: 'slideUp .4s ease-in',
       },
+      backgroundImage: {
+        'gradient': 'linear-gradient(223deg, #67C3F3 0%, #5A98F2 100%)',
+      },
     },
     plugins: [],
   },
